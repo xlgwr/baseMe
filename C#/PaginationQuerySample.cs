695c9945e7b28096e4e1fac78d2a30a4d47db59b
xlgwr <xlgwr@163.com>
2019-07-30 10:21:34 +0800

    Update PaginationQuerySample.cs

--- C#/PaginationQuerySample.cs
+++ C#/PaginationQuerySample.cs
@@ -1,61 +1,72 @@
- public async Task<JsonResult> Index(PaginationQuery pQuery)
+public string sqlIndex
+{
+    get
+    {
+        return @" 
+        with allData as( 
+            SELECT Id,* FROM XX  
+        )
+       ";
+    }
+}
+public async Task<JsonResult> Index(PaginationQuery pQuery)
+{
+    int total = 0;
+
+    string where = "";
+    string likeValue = pQuery.where;
+    var valuelist = pQuery.wherelist;
+    var strSQlb = new StringBuilder();        
+    int firstFalg = 0;
+    if (valuelist.Count > 0)
+    {
+        foreach (var item in valuelist)
         {
-            int total = 0;
-
-            string where = "";
-            string likeValue = pQuery.where;
-            var valuelist = pQuery.wherelist;
-            var strSQlb = new StringBuilder();        
-            int firstFalg = 0;
-            if (valuelist.Count > 0)
+            int hasAnd = 0;
+            if (likeValue.isNull() && firstFalg == 0)
             {
-                foreach (var item in valuelist)
-                {
-                    int hasAnd = 0;
-                    if (likeValue.isNull() && firstFalg == 0)
-                    {
-                        hasAnd = 0;
-                    }
-                    else
-                    {
-                        hasAnd = item.andorFlag <= 0 ? 1 : item.andorFlag;
-                    }
-
-                    switch (item.andLikeFlag)
-                    {
-                        case 3:
-                            strSQlb.AppendLine(item.colName.toAndOrSql(item.colValue, hasAnd));
-                            break;
-                        default:
-                            strSQlb.AppendLine(item.colName.toLikeSql(item.colValue, hasAnd, item.andLikeFlag));
-                            break;
-                    }
-                    firstFalg++;
-                }
-
+                hasAnd = 0;
+            }
+            else
+            {
+                hasAnd = item.andorFlag <= 0 ? 1 : item.andorFlag;
             }
 
-            where = strSQlb.ToString();
-
-            var houseList = await BaseComm.GetPageEntitiessync<ZS_CheckInfoDto>(
-                pQuery.pagesize,
-                pQuery.pageindex,
-                sqlIndex,
-                where,
-                "Id"
-                );
-            if (houseList.Count() > 0)
+            switch (item.andLikeFlag)
             {
-                total = houseList.First().total;
+                case 3:
+                    strSQlb.AppendLine(item.colName.toAndOrSql(item.colValue, hasAnd));
+                    break;
+                default:
+                    strSQlb.AppendLine(item.colName.toLikeSql(item.colValue, hasAnd, item.andLikeFlag));
+                    break;
             }
-            PaginationData data = new PaginationData()
-            {
-                pageindex = pQuery.pageindex,
-                pagesize = pQuery.pagesize,
-                total = total,
-                rows = houseList
-            };
-            JsonMessage msg = JsonHandler.CreateMessage((int)MsgTypeEnum.成功, "", data);
-
-            return ToJsonResult(msg);
+            firstFalg++;
         }
+
+    }
+
+    where = strSQlb.ToString();
+
+    var houseList = await BaseComm.GetPageEntitiessync<ZS_CheckInfoDto>(
+        pQuery.pagesize,
+        pQuery.pageindex,
+        sqlIndex,
+        where,
+        "Id"
+        );
+    if (houseList.Count() > 0)
+    {
+        total = houseList.First().total;
+    }
+    PaginationData data = new PaginationData()
+    {
+        pageindex = pQuery.pageindex,
+        pagesize = pQuery.pagesize,
+        total = total,
+        rows = houseList
+    };
+    JsonMessage msg = JsonHandler.CreateMessage((int)MsgTypeEnum.成功, "", data);
+
+    return ToJsonResult(msg);
+}
